f9034e9c03c0347c918c3f5c6e70eccb92720889
John Cardona <alex-careto-83@hotmail.com>
2026-05-16 17:42:24 -0500

    ajustes pytest

--- ping/cliente.py
+++ ping/cliente.py
@@ -1,54 +1,5 @@
 import requests
-import time
-from datetime import datetime
 
-URL = "http://localhost/health"
-
-RETRY_DELAY = 2
-
-while True:
-
-    try:
-
-        print(f"[{datetime.now()}] Enviando Healtcheck")
-
-        response = requests.get(
-            URL,
-            timeout=5
-        )
-
-        print(
-            f"[{datetime.now()}] "
-            f"Status={response.status_code} "
-            f"Response={response.text}"
-        )
-
-        # espera normal
-        time.sleep(1)
-
-    except requests.exceptions.ConnectionError:
-
-        print(
-            f"[{datetime.now()}] "
-            f"Servidor abajo. Reintentando..."
-        )
-
-        time.sleep(RETRY_DELAY)
-
-    except requests.exceptions.Timeout:
-
-        print(
-            f"[{datetime.now()}] "
-            f"Tiempo agotado esperando response. Reintentando..."
-        )
-
-        time.sleep(RETRY_DELAY)
-
-    except Exception as e:
-
-        print(
-            f"[{datetime.now()}] "
-            f"Error inesperado: {e}"
-        )
-
-        time.sleep(RETRY_DELAY)
+def enviar_healthcheck(url):
+    response = requests.get(url, timeout=5)
+    return response.status_code, response.text
---
+++ ping/tests/test_cliente.py
@@ -0,0 +1,18 @@
+from cliente import enviar_healthcheck
+import requests
+
+def test_enviar_healthcheck(monkeypatch):
+
+    class FakeResponse:
+        status_code = 200
+        text = "OK"
+
+    def fake_get(*args, **kwargs):
+        return FakeResponse()
+
+    monkeypatch.setattr(requests, "get", fake_get)
+
+    status, text = enviar_healthcheck("http://prueba.com/health")
+
+    assert status == 200
+    assert text == "OK"
