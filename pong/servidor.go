fbc72aeaad5402cb8219dcfdbfd55d4c02f868f2
John Cardona <alex-careto-83@hotmail.com>
2026-05-16 16:04:54 -0500

    Cambio de puerto tanto en cliente como servidor

--- pong/servidor.go
+++ pong/servidor.go
@@ -60,7 +60,7 @@ func main() {
 	fs := http.FileServer(http.Dir("./src/frontend"))
 	http.Handle("/", fs)
 
-	fmt.Println("Servidor arriba en http://localhost:8080")
+	fmt.Println("Servidor arriba en http://localhost")
 
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
